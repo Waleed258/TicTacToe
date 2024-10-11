a088ae523feca78a9af407f057ee2e4e096f2c7d
Waleed <waleedd258@gmail.com>
2024-10-11 19:27:37 +0300

    add result outcome

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ let options = new Array(9).fill("");
 let player1Score = 0;
 let player2Score = 0;
 let turn = true;
-let win = false;
+let count = 0;
+let win= false;
+let draw = false;
 let winners = [];
+let xWon = false;
+let oWon = false;
 
 $('.cell').click( function() {
     let id = parseInt($(this).attr("id").split('-')[1]);
-    if(win)
+    if(win || draw)
     {
         return;
     }
@@ -27,14 +31,19 @@ $('.cell').click( function() {
         if(turn) {
             $(this).text('X');
             options[id] = 'X';
+            count++;
             turn=false;
         }
         else {
             $(this).text('O');
             options[id] = 'O';
+            count++;
             turn = true;
         }
-        checkWin();
+        
+            checkWin();
+            checkDraw();
+        
     }
 });
 $('.restart-btn').click( () => {
@@ -45,28 +54,61 @@ $('.restart-btn').click( () => {
     });
     winners = [];
     win=false;
+    draw=false;
+    count = 0;
+    $('#out-come-box').hide();
+    xWon = false;
+    oWon = false;
 });
 function checkWin() {
     winningCombinations.forEach(array => {
         if (options[array[0]] === options[array[1]] && options[array[1]] === options[array[2]] && options[array[0]] !== "")
         {   console.log(array[0]);
-           
             win = true;
             console.log("won");
             array.forEach(i => {
                 winners.push(i);
                 $(`#cell-${i}`).addClass('winner');
-
             });
             console.log(winners);
             if(options[array[0]] === 'X') {
                 player1Score++;
                 $('#player1-score').text(player1Score);
+                xWon=true;
+                result();
+                return true;
             }
             else {
                 player2Score++;
                 $('#player2-score').text(player2Score);
+                oWon=true;
+                result();
+                return true;
             }
         }
     });
+}
+function checkDraw() {
+    if(count===9 && !win) {
+        draw=true;
+        result();
+        return true;
+    }
+}
+function result() {
+    if(win || draw)
+    {  $('#out-come-box').show();
+        if(xWon)
+        {
+            $('#out-come').text("Player 1 Won!");
+        }
+        else if(oWon)
+        {
+            $('#out-come').text("Player 2 Won!");
+        }
+        else
+        {
+            $('#out-come').text("Its a Draw");
+        }
+    }
 }
